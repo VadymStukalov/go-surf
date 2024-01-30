a96b963d22ce0642a224ef438895cf6a31e93586
Vadym Stukalov <vadym7@ukr.net>
2024-01-30 23:54:46 +0200

    begin surf section and made bottom slider for this section

--- app/js/script.js
+++ app/js/script.js
@@ -6,11 +6,21 @@ $(function () {
       '<img class="slider-arrows slider-arrows__left" src="./app/images/icons/arrow-left.svg" alt="arrow" />',
     nextArrow:
       '<img class="slider-arrows slider-arrows__right" src="./app/images/icons/arrow-right.svg" alt="arrow" />',
-    asNavFor: ".slider-dots",
+    asNavFor: ".slider-dotshead",
   });
-  $(".slider-dots").slick({
+  $(".slider-dotshead").slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     asNavFor: ".header__slider",
+  });
+
+  $(".surf-slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow:
+      '<img class="slider-arrows slider-arrows__left" src="./app/images/icons/arrow-left.svg" alt="arrow" />',
+    nextArrow:
+      '<img class="slider-arrows slider-arrows__right" src="./app/images/icons/arrow-right.svg" alt="arrow" />',
+    // asNavFor: ".slider-dots",
   });
 });
